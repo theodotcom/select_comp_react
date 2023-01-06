79634c92bf7d8b63682dd5287f99769e6c6e97c2
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-06 14:42:11 +0100

    :rocket:

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { Select } from "./lib";
+import { useState } from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const [department, setDepartment] = useState('Engineering')
+  const departments = [
+    { label: 'Sales', value: 'Sales' },
+    { label: 'Marketing', value: 'Marketing' },
+    { label: 'Engineering', value: 'Engineering' },
+    { label: 'Human Resources', value: 'Human Resources' },
+    { label: 'Legal', value: 'Legal' },
+]
+  return (
+  <div style={{ width: 640, margin: "15px auto" }}>
+    <h1>Hello React</h1>
+    <Select   items={departments}
+                                onChange={(department) =>
+                                    setDepartment(department)
+                                }
+                                selected={department} />
+  </div>)
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/lib/Select.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Select.css";
+
+const Select = ({ items, onChange, selected }) => {
+  return (
+      <select
+          defaultValue={selected}
+          onChange={(e) => onChange(e.target.value)}
+      >
+          {items.map((i) => (
+              <option key={i.value} value={i.value}>
+                  {i.label}
+              </option>
+          ))}
+      </select>
+  )
+}
+
+export default Select
