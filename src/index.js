32760ce62a1556d697989313d8dd3d164077a8c7
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-09 19:57:51 +0100

    :wrench:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-import {Select}  from "./lib/Select.js";
+import {Select}  from "../dist/lib/Select.js";
 import { useState } from 'react'
 
 
